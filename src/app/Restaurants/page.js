a863cdf4bedca08c132cc5150e5d8ed3b2982709
stevejay675 <stevejay675@gmail.com>
2024-09-25 13:24:48 +0100

    second updates

--- src/app/Restaurants/page.js
+++ src/app/Restaurants/page.js
@@ -1,6 +1,5 @@
 import Banner from "@/components/banner/banner";
 import Footer from "@/components/footer/Footer";
-import MenuCategory from "@/components/menu_category/menu_categ";
 import Navbar from "@/components/navbar/Navbar";
 import Newsletter from "@/components/newsletter/newsletter";
 import RestaurantList from "@/components/restaurant_list/retaurant_list";
@@ -10,8 +9,7 @@ export default function Restaurants () {
         <div className="restaurant-page container">
            <Navbar />
            <Banner label='RESTAURANTS' imageSrc='images/banner2.png'/>
-           <MenuCategory list1='All' list2='Near by' list3='Popular' list4='Other' />
-           <RestaurantList />
+\           <RestaurantList />
            <Newsletter />
            <Footer />
         </div>
